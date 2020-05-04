927688f0a08b7b76a0e82024140796a1cc2d8633
sudo-rudra <a21.ksingh@gmail.com>
2020-05-04 15:21:13 +0530

    refresh

--- pages/india-data.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$contents_india = fopen("https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true", "r");
-$json_india = stream_get_contents($contents_india);
-fclose($contents_india);
-
-print $json_india;
-?>
